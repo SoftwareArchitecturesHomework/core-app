2d1d4c11b0f5b70d3e9657f93383f1bffebcb735
Bertók Dániel <bertokd57@gmail.com>
2025-11-29 00:52:09 +0100

    group views by managers

--- prisma/migrations/20251128170247_create_reporting_views/migration.sql
+++ prisma/migrations/20251128170247_create_reporting_views/migration.sql
@@ -2,6 +2,8 @@
 
 CREATE OR REPLACE VIEW "project_employee_total_hours" AS
 SELECT
+    "M"."id" AS "managerId",
+    "M"."name" AS "managerName",
     "P"."id" AS "projectId",
     "P"."name" AS "projectName",
     "U"."id" AS "userId",
@@ -15,21 +17,33 @@ INNER JOIN
     "TimeEntry" AS "TE" ON "T"."id" = "TE"."taskId"
 INNER JOIN
     "User" AS "U" ON "TE"."userId" = "U"."id"
+INNER JOIN
+    "User" AS "M" ON "P"."ownerId" = "M"."id" -- JOIN to get Manager details
 GROUP BY
-    "P"."id", "P"."name", "U"."id", "U"."name"
+    "M"."id", "M"."name", "P"."id", "P"."name", "U"."id", "U"."name"
 ORDER BY
-    "P"."name", "U"."name";
+    "M"."name", "P"."name", "U"."name";
 
 CREATE OR REPLACE VIEW "avg_completed_project_duration" AS
 SELECT
+    "M"."id" AS "managerId",
+    "M"."name" AS "managerName",
     AVG(DATE_PART('day', "P"."endDate"::timestamp - "P"."startDate"::timestamp)) AS "averageProjectDurationDays"
 FROM
     "Project" AS "P"
+INNER JOIN
+    "User" AS "M" ON "P"."ownerId" = "M"."id" -- JOIN to get Manager details
 WHERE
-    "P"."endDate" IS NOT NULL;
+    "P"."endDate" IS NOT NULL
+GROUP BY
+    "M"."id", "M"."name"
+ORDER BY
+    "M"."name";
 
 CREATE OR REPLACE VIEW "project_duration_variance" AS
 SELECT
+    "M"."id" AS "managerId",
+    "M"."name" AS "managerName",
     "P"."id" AS "projectId",
     "P"."name" AS "projectName",
     DATE_PART('day', "P"."plannedEndDate"::timestamp - "P"."startDate"::timestamp) AS "plannedDurationDays",
@@ -39,13 +53,17 @@ SELECT
     DATE_PART('day', "P"."plannedEndDate"::timestamp - "P"."startDate"::timestamp) AS "durationVarianceDays"
 FROM
     "Project" AS "P"
+INNER JOIN
+    "User" AS "M" ON "P"."ownerId" = "M"."id" -- JOIN to get Manager details
 WHERE
     "P"."endDate" IS NOT NULL AND "P"."plannedEndDate" IS NOT NULL
 ORDER BY
-    "durationVarianceDays" DESC;
+    "managerName", "durationVarianceDays" DESC;
 
 CREATE OR REPLACE VIEW "monthly_project_hours" AS
 SELECT
+    "M"."id" AS "managerId",
+    "M"."name" AS "managerName",
     TO_CHAR("TE"."date", 'YYYY-MM') AS "month",
     "P"."id" AS "projectId",
     "P"."name" AS "projectName",
@@ -56,7 +74,9 @@ INNER JOIN
     "Task" AS "T" ON "TE"."taskId" = "T"."id"
 INNER JOIN
     "Project" AS "P" ON "T"."projectId" = "P"."id"
+INNER JOIN
+    "User" AS "M" ON "P"."ownerId" = "M"."id" -- JOIN to get Manager details
 GROUP BY
-    "month", "P"."id", "P"."name"
+    "M"."id", "M"."name", "month", "P"."id", "P"."name"
 ORDER BY
-    "month" DESC, "totalMonthlyHours" DESC;
+    "M"."name", "month" DESC, "totalMonthlyHours" DESC;
