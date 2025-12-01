c24f0253af8e2d833fb4907dcc148c8cdd70dd9e
WingSMC <gergely089@gmail.com>
2025-12-01 23:56:13 +0100

    corrections

--- types/event-creation-dto.ts
+++ types/event-creation-dto.ts
@@ -1,12 +1,10 @@
-import * as string_decoder from "node:string_decoder";
-
 export interface EventCreationDto {
-    type:string
-    name:string | null
-    description:string | null
-    projectId: number | null
-    startDate: string
-    endDate: string
-    participantIds: []
-    taskId: number | null
+  type: string
+  name: string | null
+  description: string | null
+  projectId: number | null
+  startDate: string
+  endDate: string
+  participantIds: number[]
+  taskId: number | null
 }
